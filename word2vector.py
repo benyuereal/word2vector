61a005b88bc967fb5fe18045742d34344ed9b588
Mac <Mac@ben.local>
2018-10-10 09:45:52 +0800

    计算应用模型，预测单词

--- word2vector.py
+++ word2vector.py
@@ -9,23 +9,16 @@ import jieba
 
 
 class word2vector():
-    def __init__(self,
-                 # 下面是各种变量
-                 vector_length=15000,
-                 # 学习率
-                 learn_rate=0.025,
-                 # 窗口大小
-                 window_length=5,
-
-                 ):
+    def __init__(self, vector_length=15000, learn_rate=0.025, window_length=5, model='cbow'):
         # 再定义其他变量
         # 这个是去掉停用词之后的词顺序表
-        self.cut_text_list = None,
-        self.vector_length = vector_length,
-        self.learn_rate = learn_rate,
-        self.window_length = window_length,
+        self.model = model
+        self.cut_text_list = None
+        self.vector_length = vector_length
+        self.learn_rate = learn_rate
+        self.window_length = window_length
         # 这个是字典 字典里面存储的是该单词的哈夫曼编码、出现的频率、出现的频率
-        self.word_dictionary = None,
+        self.word_dictionary = None  # each element is a dict, including: word,possibility,vector,huffmancode
         # 哈夫曼树
         self.huffman_tree = None
 
@@ -33,13 +26,15 @@ class word2vector():
     # fixme 大体思路:切分单词统计出词频（包括去掉部分单词）、训练模型、保存模型、进行预测
     def train_model(self, text_list):
         if self.huffman_tree == None:
+            print(self.word_dictionary)
             if self.word_dictionary == None:
                 # 统计出词频
                 word_counts = word_count(text_list)
+                self.cut_text_list = word_counts.text_list
                 # 构造字典
-                self.generate_word_dictionary(word_counts.count_result.large_than(5))
+                self.generate_word_dictionary(word_counts.count_result.larger_than(1))
             # 产生一个全量的哈夫曼树
-            self.huffman_tree = huffman_tree(None, self.word_dictionary, vector_length=self.vector_length)
+            self.huffman_tree = huffman_tree(self.word_dictionary, vector_length=self.vector_length)
         # 以上，最重要的数据环节 哈夫曼树、单词词频已经产生好了，接下来进行训练
         print('word_dict and huffman tree already generated, ready to train vector')
         # 设置待训练的单词在窗口中左右两边的单词长度,一般情况下窗口是奇数，比如 11 左边是5 右边也是5
@@ -115,13 +110,14 @@ class word2vector():
         node = root
         e = np.zeros([1, self.vector_length])
         # 假如哈夫曼编码是 1001 也就是 '左右右左'。
+        # 以下是每个节点
         for level in range(huffman_code.__len__()):
             huffman_charat = huffman_code[level]
             # 判别正类和负类的方法是使用sigmoid函数
             q = self.__Sigmoid(input_vector.dot(node.huffman_vector.T))
             # 梯度公式 ∂L∂xw=∑j=2lw(1−dwj−σ(xTwθwj−1))θwj−1
             grad = self.learn_rate * (1 - int(huffman_charat) - q)
-            # e是输出向量 是将目标单词分类的向量
+            # e是输出向量 是将目标单词分类的向量 用来更新xw
             e += grad * node.huffman_vector
             # 更新node的内部哈夫曼向量
             node.huffman_vector += grad * input_vector
@@ -145,6 +141,7 @@ class word2vector():
             #        [ 1.        ,  0.        ,  0.        ],
             #        [ 0.        ,  0.70710678, -0.70710678]])
             node.huffman_vector = preprocessing.normalize(node.huffman_vector)
+            # 0 向右边 1 向左边
             if huffman_charat == '0':
                 node = node.right
             else:
@@ -187,7 +184,7 @@ class word2vector():
                     huffman_code=None
                 )
                 word_dictionary[item[0]] = temp_dict
-        self.word_dictionary = word_frequent
+        self.word_dictionary = word_dictionary
 
 
 # 定义哈夫曼节点对象
@@ -201,18 +198,18 @@ class huffman_node():
                  is_leaf=False,
                  ):
         # 定义哈夫曼当前节点是从上个节点的左拐还是右拐
-        self.huffman_direction = None,
+        self.huffman_direction = None
         # 定义哈夫曼节点对象中的向量θ
-        self.huffman_vector = None,
+        self.huffman_vector = None
         # 定义哈夫曼树的左子树和右子树
-        self.left = None,
-        self.right = None,
+        self.left = None
+        self.right = None
         # 定义是否是叶子节点
-        self.is_leaf = is_leaf,
+        self.is_leaf = is_leaf
         # 定义哈夫曼节点对象中的哈夫曼编码
-        self.huffman_code = '',
+        self.huffman_code = ''
         self.value = value
-        self.huffman_possibility = huffman_possibility,
+        self.huffman_possibility = huffman_possibility
 
 
 # 利用了归并排序
@@ -262,22 +259,20 @@ class huffman_tree():
                  word_dictionary,
                  vector_length=15000,
                  # 然后有一个代表着当前的单词个数
-                 word_length=None,
-
                  ):
 
         self.root = None
 
         # 下面有几个值是需要在传入初始化变量进行计算的
         # 首先是根据传入的字典表 构造成一个字典顺序表
-        word_dictionary_list = list(word_dictionary.values)
+        word_dictionary_list = list(word_dictionary.values())
         self.word_dictionary_list = word_dictionary_list
         # 然后是向量个数
         self.vector_length = vector_length
         # 然后将每个节点都初始化放进去
         node_list = [huffman_node(x['word'], x['possibility'], True) for x in word_dictionary_list]
         # 以上，算是将各个节点的放进顺序表里面，下面还会产生哈夫曼结构、哈夫曼编码
-        self.build_huffman_root(node_list)
+        # self.build_huffman_root(node_list)
         # 组装一个哈夫曼树形结构
         self.build_huffman_tree(node_list)
         # 产生哈夫曼编码
@@ -290,7 +285,7 @@ class huffman_tree():
                            node_list
                            ):
         first = 0
-        end = node_list.__len__()
+        end = node_list.__len__() - 1
         # 其实就是寻找这个顺序表里面possibility最小的那个
         while node_list.__len__() > 1:
             if first >= end:
@@ -305,57 +300,56 @@ class huffman_tree():
     # 思路：每两个node作为一个节点
     def build_huffman_tree(self, node_list):
         current_node_list = []
+        node_list.sort(key=lambda x: x.huffman_possibility, reverse=True)
 
         while len(node_list) != 1:
             # 最小的
-            minimal = node_list[0]
+            minimal = node_list[-1]
             # 次小的
-            minor = node_list[1]
+            minor = node_list[-2]
             root = self.merge_left_right_and_root(minimal, minor)
-            node_list.append(root)
             # 将已经使用过的树的部分删除掉
-            node_list.pop(0)
-            node_list.pop(0)
+            node_list.pop(-1)
+            node_list.pop(-1)
+            node_list.append(root)
             # 按照节点的possibility进行重新排序
-            node_list.sort(key=lambda x: x.possibility, reverse=True)
+            node_list.sort(key=lambda x: x.huffman_possibility, reverse=True)
         # 倒数第一个 也仅有一个是树
         self.root = node_list[-1]
 
     # 产生哈夫曼编码 利用递归
     def generate_huffman_code(self, root, word_dictionary):
 
-        if root is None:
+        # 到叶子节点就结束了
+        if root.is_leaf is True:
+            word_dictionary[root.value]['huffman_code'] = root.huffman_code
             return
         else:
-            # 如果当前的点不是叶子节点
             # 根节点
-            while root.is_leaf is False:
-                # TODO 这里用到的就是 哈夫曼编码
-                code = root.huffman_code
-                # 如果是最上面的根节点 那么它的huffman是code
-                left_node = root.left
-                # 左边是 1 右边是 0
-                if left_node != None:
-                    left_code = code + '1'
-                    left_node.huffman_code = left_code
-                    # TODO 在上面 temp_dict中
-                    word_dictionary[left_node.value]['huffman_code'] = left_code
-                    # 然后再判定一下 如果左 或者 右 是叶子 那么久赋值
-                    self.generate_huffman_code(left_node)
-                right_node = root.right
-                if right_node != None:
-                    right_code = code + '0'
-                    right_node.huffman_code = right_code
-                    word_dictionary[right_node.value]['huffman_code'] = right_code
-                    self.generate_huffman_code(right_node)
+            # TODO 这里用到的就是 哈夫曼编码
+            code = root.huffman_code
+            # 如果是最上面的根节点 那么它的huffman是code
+            left_node = root.left
+            # 左边是 1 右边是 0
+            if left_node != None:
+                left_code = code + '1'
+                left_node.huffman_code = left_code
+                # 然后再判定一下 如果左 或者 右 是叶子 那么久赋值
+                self.generate_huffman_code(left_node, word_dictionary)
+            right_node = root.right
+            if right_node != None:
+                right_code = code + '0'
+                right_node.huffman_code = right_code
+                # 只有叶子节点在字典表里面才能找得到
+                self.generate_huffman_code(right_node, word_dictionary)
 
     # 构造出当前这一对叶子节点与根节点的数对
     def merge_left_right_and_root(self, node1, node2):
-        root_possibility = node1.possibility + node2.possibility
+        root_possibility = node1.huffman_possibility + node2.huffman_possibility
         root = huffman_node(None, root_possibility, False)
         # 初始化θ
         root.huffman_vector = np.zeros([1, self.vector_length])
-        if node1.possibility > node2.possibility:
+        if node1.huffman_possibility > node2.huffman_possibility:
             # 大的都放左边
             root.left = node1
             root.right = node2
@@ -418,48 +412,88 @@ class mul_counter(Counter):
     def __init__(self, element_list):
         super().__init__(element_list)
 
-        def larger_than(self, minvalue, ret='list'):
-            temp = sorted(self.items(), key=_itemgetter(1), reverse=True)
-            low = 0
-            high = temp.__len__()
-            while (high - low > 1):
-                mid = (low + high) >> 1
-                if temp[mid][1] >= minvalue:
-                    low = mid
-                else:
-                    high = mid
-            if temp[low][1] < minvalue:
-                if ret == 'dict':
-                    return {}
-                else:
-                    return []
-            if ret == 'dict':
-                ret_data = {}
-                for ele, count in temp[:high]:
-                    ret_data[ele] = count
-                return ret_data
+    # minvalue 是这个单词最低出现的频率
+    def larger_than(self, minvalue, ret='list'):
+        temp = sorted(self.items(), key=_itemgetter(1), reverse=True)
+        low = 0
+        high = temp.__len__()
+        while (high - low > 1):
+            mid = (low + high) >> 1
+            if temp[mid][1] >= minvalue:
+                low = mid
             else:
-                return temp[:high]
+                high = mid
+        if temp[low][1] < minvalue:
+            if ret == 'dict':
+                return {}
+            else:
+                return []
+        if ret == 'dict':
+            ret_data = {}
+            for ele, count in temp[:high]:
+                ret_data[ele] = count
+            return ret_data
+        else:
+            return temp[:high]
 
-        def less_than(self, maxvalue, ret='list'):
-            temp = sorted(self.items(), key=_itemgetter(1))
-            low = 0
-            high = temp.__len__()
-            while ((high - low) > 1):
-                mid = (low + high) >> 1
-                if temp[mid][1] <= maxvalue:
-                    low = mid
-                else:
-                    high = mid
-            if temp[low][1] > maxvalue:
-                if ret == 'dict':
-                    return {}
-                else:
-                    return []
-            if ret == 'dict':
-                ret_data = {}
-                for ele, count in temp[:high]:
-                    ret_data[ele] = count
-                return ret_data
+    def less_than(self, maxvalue, ret='list'):
+        temp = sorted(self.items(), key=_itemgetter(1))
+        low = 0
+        high = temp.__len__()
+        while ((high - low) > 1):
+            mid = (low + high) >> 1
+            if temp[mid][1] <= maxvalue:
+                low = mid
             else:
-                return temp[:high]
+                high = mid
+        if temp[low][1] > maxvalue:
+            if ret == 'dict':
+                return {}
+            else:
+                return []
+        if ret == 'dict':
+            ret_data = {}
+            for ele, count in temp[:high]:
+                ret_data[ele] = count
+            return ret_data
+        else:
+            return temp[:high]
+
+
+if __name__ == '__main__':
+    # text = FI.load_pickle('./static/demo.pkl')
+    # text =[ x['dealed_text']['left_content'][0] for x in text]
+
+    data = [
+        'Cristiano Ronaldo dos Santos Aveiro (Cristiano Ronaldo dos Santos Aveiro), born on February 5, 1985 in Funchal, Madeira, Portugal, is a professional football player in Portugal. The winger and center play for Juventus Football Club of Italy and are the captain of the Portuguese national men soccer team.Cristiano Ronaldo debuted at Sporting Lisbon. In 2003, he joined the Premier League Manchester United. He won 10 championships in the English Premier League, the UEFA Champions League and the World Club Cup. In June 2009, he was transferred to Real Madrid with a price of 96 million euros. He won 16 championships in the UEFA Champions League, 2 La Liga titles and 3 World Cup Cup championships. Cristiano Ronaldo played for Real Madrid in 9 years, contributing 450 goals and 131 assists in 438 games, and became the player with the highest scoring rate in Real Madrid history with a scoring rate of 1.03 goals per game.Cristiano Ronaldo maintains a number of personal records, including the total scores of individual scores in the five major leagues in Europe, the total score of the Real Madrid club individual goals, the total score of the Champions League individual goals, and the total scores of the European national team individual goals. Cristiano Ronaldo has won the Golden Globe Award five times, three times to win the World Footballer, four times to win the European Golden Boot Award, and seven times to win the Champions League top scorer and other personal honors.In July 2016, C Ronaldo led Portugal to the 2016 European Cup in France, the first international competition in the history of the Portuguese national team. On July 10, 2018, the transfer to Serie A Juventus. On July 24, 2018, Cristiano Ronaldo was selected as the candidate for the 2018 FIFA World Footballer. In September 2018, he was selected as the FIFA Team of the Year.']
+    word2vec = word2vector(vector_length=500)
+    word2vec.train_model(data)
+    file_interface.save_pickle(word2vec.word_dictionary, './static/word2vector.pkl')
+
+    data = file_interface.load_pickle('./static/word2vector.pkl')
+    x = {}
+    for key in data:
+        temp = data[key]['huffman_vector']
+        temp = preprocessing.normalize(temp)
+        x[key] = temp
+    file_interface.save_pickle(x, './static/normal_word2vector.pkl')
+
+    x = file_interface.load_pickle('./static/normal_word2vector.pkl')
+
+
+    def cal_simi(data, key1, key2):
+        return data[key1].dot(data[key2].T)[0][0]
+
+
+    possibility_list = []
+    keys = list(x.keys())
+    for key in keys:
+        every = huffman_node(
+            key,cal_simi(x, 'United', key))
+        possibility_list.append(every)
+
+    possibility_list.sort(key=lambda x: x.huffman_possibility, reverse=True)
+
+    for obj in possibility_list:
+        print(obj.value, '\t', obj.huffman_possibility)
+
